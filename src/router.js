b9f18fb413f5a71cb774125dfd93bcd989564ded
maatcha <mattcha54@gmail.com>
2020-03-13 18:24:38 +0100

    progress bar finished

---
+++ src/router.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate.vue'
+import EventList from './views/EventList.vue'
+import EventShow from './views/EventShow.vue'
+import NProgress from 'nprogress'
+import store from '@/store/store'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventList,
+      props: true
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    },
+    {
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true,
+      beforeEnter(routeTo, routeFrom, next) {
+        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
+          routeTo.params.event = event
+          next()
+        })
+      }
+    }
+  ]
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start()
+  next()
+})
+
+router.beforeResolve((routeTo, routeFrom, next) => {
+  if (routeTo.name === 'event-list') {
+    const currentPage = parseInt(routeTo.query.page) || 1
+    store
+      .dispatch('event/fetchEvents', {
+        page: currentPage
+      })
+      .then(() => {
+        routeTo.params.page = currentPage
+        next()
+      })
+  } else {
+    next()
+  }
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
---
+++ src/views/EventList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        Prev Page</router-link
+      >
+      <template v-if="hasNextPage"> | </template>
+    </template>
+    <router-link
+      v-if="hasNextPage"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page</router-link
+    >
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    EventCard
+  },
+  computed: {
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.event.perPage
+    },
+    ...mapState(['event', 'user'])
+  }
+}
+</script>

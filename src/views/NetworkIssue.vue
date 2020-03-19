7a59ffe7a00641aafc9b341fbfe6deaa7edf54d1
maatcha <mattcha54@gmail.com>
2020-03-19 13:29:28 +0100

    error handling ok

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Uh-Oh !</h1>
+    <h3>
+      It's look like you're experiencing some network issues. Please click the
+      Back button and try again
+    </h3>
+    <router-link :to="{ name: 'event-list' }"
+      >Or go back to the Home Page</router-link
+    >
+  </div>
+</template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Oooops !</h1>
+    <h2>The {{ resource }} you're looking for is not here</h2>
+    <router-link :to="{ name: 'event-list' }"
+      >Back to the Home Page</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
